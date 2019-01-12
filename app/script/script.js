7c46b44005f0a010ad15f2c5ca4466182ad52e11
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-01-11 18:50:46 -0800

    Open external pages inline

--- app/script/script.js
+++ app/script/script.js
@@ -13,3 +13,40 @@ window.onpageshow = function() {
     }
   });
 }
+
+const INLINE_BORDER = 50 * 2;
+
+function closeInlinePage() {
+  const div = document.querySelector(".inline-page");
+  if (div) {
+    div.remove();
+  }
+}
+
+function openInlinePage(url) {
+  closeInlinePage();
+  const builder = document.createElement('div');
+  const width = document.body.clientWidth - INLINE_BORDER;
+  const height = document.body.clientHeight - INLINE_BORDER;
+  builder.innerHTML = `<div class="inline-page"><iframe frameborder="0" border="0" width="${width}" height="${height}" src="${url}"></div>`;
+  document.body.appendChild(builder.firstElementChild);
+  resizeInlinePage();
+  const div = document.querySelector(".inline-page");
+  function noScroll(e) {
+    e.preventDefault();
+  }
+  div.addEventListener('scroll', noScroll);
+  div.addEventListener('mousewheel', noScroll);
+  div.addEventListener('click', closeInlinePage);
+}
+
+ function resizeInlinePage() {
+  const div = document.querySelector(".inline-page");
+  const frame = document.querySelector(".inline-page iframe");
+  if (div && frame) {
+    frame.width = div.clientWidth - INLINE_BORDER;
+    frame.height = div.clientHeight - INLINE_BORDER;
+  }
+}
+
+window.addEventListener('resize', resizeInlinePage);
