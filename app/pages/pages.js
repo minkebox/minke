30218214555c3ce8e51fc28627b2e403685c0080
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-01-30 00:20:17 -0800

    Centralize web setup

---
+++ app/pages/pages.js
@@ -0,0 +1,36 @@
+const FS = require('fs');
+const Path = require('path');
+
+const Pages = {
+  '/':                require('./Main'),
+  '/settings/:id':    require('./Settings'),
+  '/new/application': require('./NewApplication'),
+  '/new/network':     require('./NewNetwork')
+};
+
+function pages(root, wsroot) {
+
+  root.get('/js/:script', async (ctx) => {
+    ctx.body = FS.readFileSync(`${__dirname}/script/${ctx.params.script}`, { encoding: 'utf8' });
+    ctx.type = 'text/javascript';
+  });
+  root.get('/css/style.css', async (ctx) => {
+    ctx.body = FS.readFileSync(`${__dirname}/css/style.css`, { encoding: 'utf8' });
+    ctx.type = 'text/css';
+  });
+  root.get('/img/:img', async (ctx) => {
+    ctx.body = FS.readFileSync(`${__dirname}/img/${ctx.params.img}`);
+    ctx.type = 'image/png';
+  });
+
+  for (let key in Pages) {
+    if (Pages[key].HTML) {
+      root.get(key, Pages[key].HTML);
+    }
+    if (Pages[key].WS) {
+      wsroot.get(Path.normalize(`${key}/ws`), Pages[key].WS);
+    }
+  }
+}
+
+module.exports = pages;
