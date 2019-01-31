97b2e2dae58e8fa0943d00f5d40ee40115f7fcde
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-01-30 23:41:49 -0800

    Hide IP is app not running (IP will be old)

--- app/pages/Main.js
+++ app/pages/Main.js
@@ -10,7 +10,7 @@ function genApp(app) {
     online: app._online,
     features: app._features,
     link: app._forward && app._forward.url,
-    ip: app._homeIP,
+    ip: app._online ? app._homeIP : null,
     networks: {
       primary: app._features.vpn ? 'none' : app._networks.primary,
       secondary: app._networks.secondary
