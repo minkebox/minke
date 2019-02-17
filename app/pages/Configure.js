ec31af84b253b37a061707319ab62458ba687fac
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-02-17 00:29:54 -0500

    Include app name in shareable name

--- app/pages/Configure.js
+++ app/pages/Configure.js
@@ -112,7 +112,7 @@ async function ConfigurePageHTML(ctx) {
               return {
                 target: share.target,
                 host: share.host,
-                action: `window.action('${action.type}#${shareable.app._id}#${share.host}#${action.name}/${share.target}',this.checked)`,
+                action: `window.action('${action.type}#${shareable.app._id}#${share.host}#${action.name}/${shareable.app._name}.${share.target}',this.checked)`,
                 value: !!app._shares.find(ashare => ashare.appid === shareable.app._id && ashare.host == share.host)
               };
             })};
