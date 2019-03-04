32f1321016a1da72dd960536108547b99873f4ec
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-04 11:26:21 -0800

    Announce + unannounce

--- app/MDNS.js
+++ app/MDNS.js
@@ -2,7 +2,6 @@ const DnsPacket = require('dns-packet');
 const Dgram = require('dgram');
 const OS = require('os');
 
-const IFACE = 'br0';
 const MCAST_ADDRESS = '224.0.0.251';
 const PORT = 5353;
 
@@ -22,26 +21,26 @@ const MDNS = {
     this._create();
   },
 
-  stop: function() {
+  stop: async function() {
     if (this._socket) {
+      await this._unannounce();
       this._socket.close();
       this._socket = null;
     }
   },
 
   update: function(config) {
-    this._hostname = config.hostname;
+    if (this._socket) {
+      this._unannounce();
+      this._hostname = config.hostname;
+      this._announce();
+    }
+    else {
+      this._hostname = config.hostname;
+    }
   },
 
   _create: function() {
-    const interfaces = OS.networkInterfaces()[IFACE];
-    if (!interfaces) {
-      return false;
-    }
-    const interface = interfaces.find(i => i.family === 'IPv4');
-    if (!interface) {
-      return false;
-    }
     this._socket = Dgram.createSocket({
       type: 'udp4',
       reuseAddr: true
@@ -51,21 +50,20 @@ const MDNS = {
         pkt.questions.forEach((q) => {
           switch (q.name) {
             case SERVICES:
-              this._answer([{ name: q.name, type: 'PTR', ttl: 4500, data: HTTP }]);
+              this._answer([{ name: SERVICES, type: 'PTR', ttl: 4500, data: HTTP }]);
               break;
             case HTTP:
-              this._answer([{ name: q.name, type: 'PTR', ttl: 120/*4500*/, data: `${this._hostname}.${q.name}` }]);
+              this._answer([{ name: HTTP, type: 'PTR', data: `${this._hostname}.${HTTP}` }]);
               break;
             default:
               if (q.name === `${this._hostname}.${HTTP}`) {
                 this._answer([
-                  { name: q.name, type: 'SRV', ttl: 120,  data: { priority: 0, weight: 0, port: 80, target: `${this._hostname}.local` }},
-                  { name: q.name, type: 'TXT', ttl: 120/*4500*/, data: [] },
-                  { name: q.name, type: 'A',   ttl: 120,  data: this._ip }
+                  { name: `${this._hostname}.${HTTP}`, type: 'SRV', data: { priority: 0, weight: 0, port: 80, target: `${this._hostname}.local` }},
+                  { name: `${this._hostname}.${HTTP}`, type: 'TXT', data: [] }
                 ]);
               }
               else if (q.name === `${this._hostname}.local`) {
-                this._answer([{ name: q.name, type: 'A', ttl: 120, data: this._ip }]);
+                this._answer([{ name: `${this._hostname}.local`, type: 'A', data: this._ip }]);
               }
               break;
           }
@@ -75,23 +73,46 @@ const MDNS = {
     this._socket.bind(PORT, MCAST_ADDRESS, () => {
       this._socket.setMulticastTTL(255);
       this._socket.setMulticastLoopback(false);
-      this._socket.addMembership(MCAST_ADDRESS, interface.address);
-      this._socket.setMulticastInterface(interface.address);
+      this._socket.addMembership(MCAST_ADDRESS, this._ip);
+      this._socket.setMulticastInterface(this._ip);
+      this._announce();
     });
     return true;
   },
 
-  _answer: function(answers) {
-    const msg = DnsPacket.encode({
-      type: 'response',
-      questions: [],
-      authorities: [],
-      additionals: [],
-      answers: answers.map((answer) => {
-        return Object.assign({ class: 'IN', flush: false }, answer);
-      })
+  _announce: async function() {
+    await this._answer([
+      { name: HTTP,                        type: 'PTR', data: `${this._hostname}.${HTTP}` },
+      { name: `${this._hostname}.${HTTP}`, type: 'SRV', data: { priority: 0, weight: 0, port: 80, target: `${this._hostname}.local` }},
+      { name: `${this._hostname}.${HTTP}`, type: 'TXT', data: [] },
+      { name: `${this._hostname}.local`,   type: 'A',   data: this._ip }
+    ]);
+  },
+
+  _unannounce: async function() {
+    await this._answer([
+      { name: HTTP,                        ttl: 0, type: 'PTR', data: `${this._hostname}.${HTTP}` },
+      { name: `${this._hostname}.${HTTP}`, ttl: 0, type: 'SRV', data: { priority: 0, weight: 0, port: 80, target: `${this._hostname}.local` }},
+      { name: `${this._hostname}.${HTTP}`, ttl: 0, type: 'TXT', data: [] },
+      { name: `${this._hostname}.local`,   ttl: 0, type: 'A',   data: this._ip }
+    ]);
+  },
+
+  _answer: async function(answers) {
+    return new Promise((resolve) => {
+      const msg = DnsPacket.encode({
+        type: 'response',
+        questions: [],
+        authorities: [],
+        additionals: [],
+        answers: answers.map((answer) => {
+          return Object.assign({ class: 'IN', flush: false, ttl: 120 }, answer);
+        })
+      });
+      this._socket.send(msg, 0, msg.length, PORT, MCAST_ADDRESS, () => {
+        resolve(true);
+      });
     });
-    this._socket.send(msg, 0, msg.length, PORT, MCAST_ADDRESS);
   }
 
 };
