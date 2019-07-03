6261a78f81a4f17a061436549e55b183d2da03d8
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-07-03 10:50:14 -0700

    Remove debugging

--- app/DDNS.js
+++ app/DDNS.js
@@ -49,7 +49,7 @@ const DDNS = {
       if (force) {
         this._lastip = null;
       }
-      console.log('DDNS._update', this._gids.join(','));
+      //console.log('DDNS._update', this._gids.join(','));
       clearTimeout(this._pending);
       this._pending = setTimeout(() => {
         this._getExternalIP().then((ip) => {
@@ -60,7 +60,7 @@ const DDNS = {
           }
           else if (ip !== this._lastip) {
             this._lastip = ip;
-            console.log(`${DDNS_URL}?host=${this._gids.join(',')}&ip=${ip}`);
+            //console.log(`${DDNS_URL}?host=${this._gids.join(',')}&ip=${ip}`);
             HTTPS.get(`${DDNS_URL}?host=${this._gids.join(',')}&ip=${ip}`, () => {});
           }
         });
@@ -70,9 +70,9 @@ const DDNS = {
 
   _getExternalIP: async function() {
     return new Promise((resolve) => {
-      console.log('_getExternalIP');
+      //console.log('_getExternalIP');
       UPNP.getExternalIP().then((ip) => {
-        console.log('_gotExternaIP', ip);
+        //console.log('_gotExternaIP', ip);
         if (ip) {
           resolve(ip);
         }
