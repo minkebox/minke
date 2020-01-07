be498fe478dfd25532bb1d1471d9b22e6d25053b
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-01-07 00:41:22 -0800

    Fix ipv6 address in skeletons

--- app/MinkeApp.js
+++ app/MinkeApp.js
@@ -973,7 +973,7 @@ MinkeApp.prototype = {
         __APPNAME: { value: this._name },
         __GLOBALNAME: { value: `${this._globalId}${GLOBALDOMAIN}` },
         __HOMEIP: { value: this._homeIP },
-        __HOMEIP6: { value: this.getSLAACAddress() },
+        __HOMEIP6: { value: this.getSLAACAddress() ? this.getSLAACAddress().canonicalForm() : '<none>' },
         __DOMAINNAME: { value: MinkeApp.getLocalDomainName() },
         __MACADDRESS: { value: this._primaryMacAddress().toUpperCase() }
       }, this._env);
