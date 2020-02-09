72f4dcd5c1530d3a5fc8d3242652b799e3288ed2
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-02-08 22:18:58 -0800

    Move prod DDNS

--- app/Config-Production.js
+++ app/Config-Production.js
@@ -3,7 +3,7 @@ module.exports = {
   WEB_PORT: 80,
   REGISTRY_HOST: 'registry.minkebox.net',
   REGISTRY_DEFAULT_TAG: 'latest',
-  DDNS_UPDATE: 'https://minkebox.net/update',
+  DDNS_UPDATE: 'https://ddns.minkebox.net/update',
   GLOBALDOMAIN: '.minkebox.net',
   DEFAULT_FALLBACK_RESOLVER: '1.1.1.1',
   DOH_SERVER_NAME: 'doh.minkebox.net',
