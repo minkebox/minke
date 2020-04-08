1788ddd9e98ff3bd45b0c7f265315b5ef414ab54
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-04-08 15:37:50 -0700

    Add development config

--- app/Config-Development.js
+++ app/Config-Development.js
@@ -1,9 +1,9 @@
 module.exports = {
   ROOT: '/minke',
-  CONFIG_NAME: 'Production',
+  CONFIG_NAME: 'Development',
   WEB_PORT: 80,
   REGISTRY_HOST: 'registry.minkebox.net',
-  REGISTRY_DEFAULT_TAG: 'latest',
+  REGISTRY_DEFAULT_TAG: 'dev',
   DDNS_UPDATE: 'https://ddns.minkebox.net/update',
   GLOBALDOMAIN: '.minkebox.net',
   DEFAULT_FALLBACK_RESOLVER: '1.1.1.1'
