9b9f83b730699957758529efb153ac123fe078c8
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-04-13 10:10:23 -0700

    Tidy

--- app/Filesystem.js
+++ app/Filesystem.js
@@ -215,11 +215,8 @@ const Filesystem = {
     info.Mounts.forEach(mount => {
       if (mount.Type === 'bind') {
         this._mappings[mount.Destination] = { src: mount.Source, dst: mount.Destination, propagation: mount.Propagation };
-        if (mount.Destination.indexOf(NATIVE_DIR) === 0) {
-          const info = FS.statSync(mount.Destination);
-          if (info.isDirectory()) {
-            this._natives.push(this._mappings[mount.Destination]);
-          }
+        if (mount.Destination.indexOf(NATIVE_DIR) === 0 && FS.statSync(mount.Destination).isDirectory()) {
+          this._natives.push(this._mappings[mount.Destination]);
         }
       }
     });
