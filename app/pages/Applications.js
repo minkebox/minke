adff5084adb045f44443acaa46770cc0f70ab189
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-04-20 11:17:13 -0700

    Fix broken new image pull

--- app/pages/Applications.js
+++ app/pages/Applications.js
@@ -5,6 +5,7 @@ const MinkeApp = require('../MinkeApp');
 const Images = require('../Images');
 const Skeletons = require('../Skeletons');
 
+const GUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 const NRTAGS = 20;
 
 function _strhash(str) {
@@ -71,6 +72,9 @@ async function PageWS(ctx) {
               (skel.skeleton.secondary ||[]).forEach(secondary => {
                 images.push(secondary.image);
               });
+            }
+            else if (!GUID.test(msg.value)) {
+              images.push(msg.value);
             }
             const download = [];
             const extract = [];
